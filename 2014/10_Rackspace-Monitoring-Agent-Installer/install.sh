745b9a1d9d720da9cba3705fb9cb8c35e853f2ef
Andrew Mussey <MusseyAM@vt.edu>
2014-10-13 15:40:15 -0400

    Added prompts for the Username and API key.
     * Silenced apt-get update.
     * Set the checks for the distro to be general string matches instead of exact string matches.

--- 2014/10_Rackspace-Monitoring-Agent-Installer/install.sh
+++ 2014/10_Rackspace-Monitoring-Agent-Installer/install.sh
@@ -15,46 +15,69 @@ if [ "$EUID" -ne 0 ] ; then
     exit 3
 fi
 
+echo -e "This installer will require your MyCloud username and API key, found on the Account Settings page at:\n"
+echo -e "    https://mycloud.rackspace.com/account#settings\n\n"
+
+echo -n "Please enter your MyCloud username: "
+read RAX_USERNAME
+echo -n "Please enter your API Key: "
+read RAX_APIKEY
+
+echo -en "\nReady to install the monitoring agent.  Press [ENTER] to proceed."
+read
+
 if [[ "$YOUR_DISTRO" =~ "Ubuntu " ]] ; then
-    if [ "$YOUR_DISTRO" = "Ubuntu 10.04" ] ; then
+    echo "Performing apt-get update"
+    sudo apt-get update > /dev/null
+    sudo apt-get install curl -y
+    if [[ "$YOUR_DISTRO" =~ "Ubuntu 10.04" ]] ; then
         echo "Ubuntu 10.04"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-10.04-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-    elif [ "$YOUR_DISTRO" = "Ubuntu 11.04" ] ; then
+    elif [[ "$YOUR_DISTRO" =~ "Ubuntu 11.04" ]] ; then
         echo "Ubuntu 11.04"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-11.04-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-    elif [ "$YOUR_DISTRO" = "Ubuntu 11.10" ] ; then
+    elif [[ "$YOUR_DISTRO" =~ "Ubuntu 11.10" ]] ; then
         echo "Ubuntu 11.10"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-11.10-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-    elif [ "$YOUR_DISTRO" = "Ubuntu 12.04" ] ; then
+    elif [[ "$YOUR_DISTRO" =~ "Ubuntu 12.04" ]] ; then
         echo "Ubuntu 12.04"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-12.04-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-    elif [ "$YOUR_DISTRO" = "Ubuntu 12.10" ] ; then
+    elif [[ "$YOUR_DISTRO" =~ "Ubuntu 12.10" ]] ; then
         echo "Ubuntu 12.10"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-12.10-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-    elif [ "$YOUR_DISTRO" = "Ubuntu 13.04" ] ; then
+    elif [[ "$YOUR_DISTRO" =~ "Ubuntu 13.04" ]] ; then
         echo "Ubuntu 13.04"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-13.04-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-    elif [ "$YOUR_DISTRO" = "Ubuntu 13.10" ] ; then
+    elif [[ "$YOUR_DISTRO" =~ "Ubuntu 13.10" ]] ; then
         echo "Ubuntu 13.10"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-13.10-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
-    elif [ "$YOUR_DISTRO" = "Ubuntu 14.04" ] ; then
+    elif [[ "$YOUR_DISTRO" =~ "Ubuntu 14.04" ]] ; then
         echo "Ubuntu 14.04"
         sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/ubuntu-14.04-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
     fi
     curl https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc | sudo apt-key add -
-    sudo apt-get update
+    echo "Performing apt-get update"
+    sudo apt-get update > /dev/null
     sudo apt-get install rackspace-monitoring-agent -y
 elif [ "$YOUR_DISTRO" = "Debian GNU/Linux 6.0" ] ; then
     echo "Debian 6 (Squeeze)"
+    echo "Performing apt-get update"
+    sudo apt-get update > /dev/null
+    sudo apt-get install curl -y
     sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/debian-squeeze-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
     curl https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc | sudo apt-key add -
-    sudo apt-get update
+    echo "Performing apt-get update"
+    sudo apt-get update > /dev/null
     sudo apt-get install rackspace-monitoring-agent -y
 elif [ "$YOUR_DISTRO" = "Debian GNU/Linux 7" ] ; then
     echo "Debian 7 (Wheezy)"
+    echo "Performing apt-get update"
+    sudo apt-get update > /dev/null
+    sudo apt-get install curl -y
     sudo sh -c 'echo "deb http://stable.packages.cloudmonitoring.rackspace.com/debian-wheezy-x86_64 cloudmonitoring main" > /etc/apt/sources.list.d/rackspace-monitoring-agent.list'
     curl https://monitoring.api.rackspacecloud.com/pki/agent/linux.asc | sudo apt-key add -
-    sudo apt-get update
+    echo "Performing apt-get update"
+    sudo apt-get update > /dev/null
     sudo apt-get install rackspace-monitoring-agent -y
 elif [[ "$YOUR_DISTRO" =~ "Debian GNU/Linux jessie" ]] ; then
     echo "Debian Jessie"
@@ -69,3 +92,10 @@ elif [[ "$YOUR_DISTRO" =~ "Red Hat Enterprise Linux Server release 5" ]] ; then
 elif [[ "$YOUR_DISTRO" =~ "CentOS release 6.5" ]] ; then
     echo "CentOS 6.5"
 fi
+
+sudo service rackspace-monitoring-agent stop
+sudo rackspace-monitoring-agent --setup --username "$RAX_USERNAME" --apikey "$RAX_APIKEY" --production
+sudo service rackspace-monitoring-agent start
+
+echo -e "\n\nMonitoring agent install complete!  The agent should now be running on your server.\n\nHappy monitoring!"
+
